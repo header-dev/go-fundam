d2d8a320edb0b9368a15304eabdb4973ff7fd557
Wit Tech <kritawit.bunket@aia.com>
2024-02-27 23:14:40 +0700

    Completed : Interface

---
+++ interfaces/go.mod
@@ -0,0 +1,3 @@
+module demo/interfaces
+
+go 1.22.0
---
+++ interfaces/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a interface{}
+
+	a = 10
+	fmt.Printf(("%T %v\n"), a, a)
+
+	a = "hello"
+	fmt.Printf(("%T %v\n"), a, a)
+
+	a = true
+	fmt.Printf(("%T %v\n"), a, a)
+
+	a = func() string { return "hello" }
+	fmt.Printf(("%T %v\n"), a, a)
+
+}
